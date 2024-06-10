74a658d06e57b931104543ed4fb06e5e52a5e72f
vaibhavs-codewiz <vaibhavsharma000721@gmail.com>
2024-06-10 20:11:17 +0530

    onclick

---
+++ 06_bg_changer_project/learn.md
@@ -0,0 +1,4 @@
+# bg change
+in bg change project since we want to reflect changes on screen therefore it is essential to us usestate to make it possible .
+use state have function in array which is execute when button clicked.
+onclick inside button require a function here that function will be setColor .
--- 06_bg_changer_project/src/App.jsx
+++ 06_bg_changer_project/src/App.jsx
@@ -1,21 +1,21 @@
+//bg change project
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
-  let [customStyle, setCount] = useState("black");
-  function bgChange(color) {
-    customStyle = {
-      backgroundColor: color,
-    };
-    setCount(customStyle);
-  }
+  //since we need to propagate change on ui.
+  let [color, setColor] = useState("orange");
+  
+
   return (
-    <div className="card">
-      <body style={customStyle.backgroundColor}></body>
-      <button onClick={bgChange(red)}>red</button>
-      <button onClick={bgChange(blue)}>blue</button>
+    <div className="w-full h-screen" style={{ backgroundColor: color }}>
+      <div className="fixed flex flex-col gap-y-2 bg-slate-200">
+        <div className="fixed flex flex-col gap-y-2 bg-slate-100 mx-8 my-5 rounded-xl">
+          <button className="px-8 my-8 px-2 mx-4 font-semibold font-serif bg-slate-200 rounded-xl border-none shadow-2xl py-3 text-slate-700" onClick = {() => setColor("white")}>white</button>
+          <button className="px-8 my-8 px-2 mx-4 font-semibold font-serif bg-slate-400 rounded-xl border-none shadow-2xl py-3" onClick = {() => setColor("violet")}>violet</button>
+          <button className="px-8 my-8 px-2 mx-4 font-semibold font-serif bg-slate-700 rounded-xl border-none shadow-2xl py-3 text-slate-900" onClick={ () => setColor("magenta") }>magenta</button>
+          <button className="px-8 my-8 px-2 mx-4 font-semibold font-serif bg-slate-900 rounded-xl border-none shadow-2xl py-3 text-slate-400" onClick={ () => setColor("black") }>black</button>
+        </div>
+      </div>
     </div>
   );
 }
